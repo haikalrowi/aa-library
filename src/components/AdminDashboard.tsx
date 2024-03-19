58550d7a6999d7e06bf2ac56561a7607ec98d721
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-19 09:14:18 +0700

    Update AdminDashboard.tsx

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -1,14 +1,39 @@
-import { adminLogout } from "@/lib/action";
-import { Button, Card, CardBody } from "@chakra-ui/react";
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+import { Button, Tab, TabList, TabPanels, Tabs } from "@chakra-ui/react";
+
+import { AdminBook } from "./AdminBook";
+import { AdminCheckout } from "./AdminCheckout";
+import { AdminCopy } from "./AdminCopy";
+import { AdminLogout } from "./AdminLogout";
+
+export function Submit() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button type="submit" isLoading={pending}>
+      Logout
+    </Button>
+  );
+}
 
 export default function AdminDashboard() {
   return (
-    <Card>
-      <CardBody>
-        <form action={adminLogout}>
-          <Button type="submit">Logout</Button>
-        </form>
-      </CardBody>
-    </Card>
+    <Tabs isFitted className="!flex h-full flex-col">
+      <TabPanels className="flex-1 overflow-y-scroll">
+        <AdminBook />
+        <AdminCopy />
+        <AdminCheckout />
+        <AdminLogout />
+      </TabPanels>
+      <TabList className="flex-none">
+        <Tab>Book</Tab>
+        <Tab>Copy</Tab>
+        <Tab>Checkout</Tab>
+        <Tab>Logout</Tab>
+      </TabList>
+    </Tabs>
   );
 }
