d1918853d66a90dd5f5d7220a4d81e5eaf9c3aa0
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-19 08:54:17 +0700

    Update action.ts

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -13,21 +13,27 @@ export async function adminIsAdmin() {
     }
     const { id } = await verify(token);
     await prisma.admin.findUniqueOrThrow({ where: { id } });
-    return true;
   } catch (error) {
     console.error(error);
     return false;
   }
+  return true;
 }
 
 export async function adminLogin(formData: FormData) {
-  const admin = await prisma.admin.findUniqueOrThrow({
-    where: {
-      username: formData.get("username") as string,
-      password: formData.get("password") as string,
-    },
-  });
-  const token = await sign({ id: admin.id });
+  let token;
+  try {
+    const admin = await prisma.admin.findUniqueOrThrow({
+      where: {
+        username: formData.get("username") as string,
+        password: formData.get("password") as string,
+      },
+    });
+    token = await sign({ id: admin.id });
+  } catch (error) {
+    console.error(error);
+    return;
+  }
   cookies().set("token", token);
 }
 
