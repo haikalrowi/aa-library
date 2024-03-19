5a6cf2c585ed459caaecb0211ac6ff9c18d5304e
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-19 11:30:54 +0700

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,20 @@
 import AdminDashboard from "@/components/AdminDashboard";
 import AdminLogin from "@/components/AdminLogin";
 import { adminIsAdmin } from "@/lib/action";
+import prisma from "@/lib/prisma";
+
+function Login() {
+  return <AdminLogin />;
+}
+
+async function Dashboard() {
+  const books = await prisma.book.findMany();
+
+  return <AdminDashboard books={books} />;
+}
 
 export default async function Admin() {
   const isAdmin = await adminIsAdmin();
 
-  return isAdmin ? <AdminDashboard /> : <AdminLogin />;
+  return isAdmin ? <Dashboard /> : <Login />;
 }
