2cccec897933a36f3a71fabf3ccaac9db12275a6
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-19 22:32:51 +0700

    Update page.tsx

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -10,8 +10,20 @@ function Login() {
 async function Dashboard() {
   const books = await prisma.book.findMany();
   const copies = await prisma.copy.findMany({ include: { Book: true } });
+  const checkouts = await prisma.checkout.findMany({
+    include: {
+      Student: { select: { id: true, email: true, name: true } },
+      Copy: { include: { Book: true } },
+    },
+  });
+  const students = await prisma.student.findMany({
+    select: { id: true, email: true, name: true },
+  });
+  const copiesAvailable = await prisma.copy.findMany({
+    where: { available: true },
+  });
 
-  return <AdminDashboard books={books} copies={copies} />;
+  return <AdminDashboard data={{ books, copies }} />;
 }
 
 export default async function Admin() {
