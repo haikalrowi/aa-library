aab05d69cb51f2963121e340f074a21d74667a34
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-19 23:43:10 +0700

    Update Admin.ts

--- src/context/Admin.ts
+++ src/context/Admin.ts
@@ -1,19 +1,34 @@
 import { createContext } from "react";
 
-import { useAdminBookState, useAdminCopyState } from "@/hooks/Admin";
+import {
+  useAdminBookState,
+  useAdminCheckoutState,
+  useAdminCopyState,
+} from "@/hooks/Admin";
 import { Prisma } from "@prisma/client";
 
 export type AdminData = Partial<{
   books: Prisma.BookGetPayload<{}>[];
-  copies: Prisma.CopyGetPayload<{
+  copies: Prisma.CopyGetPayload<{ include: { Book: true } }>[];
+  checkouts: Prisma.CheckoutGetPayload<{
     include: {
-      Book: true;
+      Student: { select: { id: true; email: true; name: true } };
+      Copy: { include: { Book: true } };
     };
   }>[];
+  students: Prisma.StudentGetPayload<{
+    select: { id: true; email: true; name: true };
+  }>[];
+  copiesAvailable: Prisma.CopyGetPayload<{ include: { Book: true } }>[];
 }>;
 export type AdminBookState = ReturnType<typeof useAdminBookState> | undefined;
 export type AdminCopyState = ReturnType<typeof useAdminCopyState> | undefined;
+export type AdminCheckoutState =
+  | ReturnType<typeof useAdminCheckoutState>
+  | undefined;
 
 export const AdminDataContext = createContext<AdminData>({});
 export const AdminBookStateContext = createContext<AdminBookState>(undefined);
 export const AdminCopyStateContext = createContext<AdminCopyState>(undefined);
+export const AdminCheckouteStateContext =
+  createContext<AdminCheckoutState>(undefined);
