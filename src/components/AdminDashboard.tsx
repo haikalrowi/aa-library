c5327c8fa142e054da3db3ad1ef2ee9d5cae6902
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-20 09:06:27 +0700

    Update AdminDashboard.tsx

--- src/components/AdminDashboard.tsx
+++ src/components/AdminDashboard.tsx
@@ -3,10 +3,10 @@
 import { AdminData, AdminDataContext } from "@/context/Admin";
 import { Tab, TabList, TabPanels, Tabs } from "@chakra-ui/react";
 
-import { AdminBook } from "./AdminBook";
-import { AdminCheckout } from "./AdminCheckout";
-import { AdminCopy } from "./AdminCopy";
-import { AdminLogout } from "./AdminLogout";
+import AdminBook from "./AdminBook";
+import AdminCheckout from "./AdminCheckout";
+import AdminCopy from "./AdminCopy";
+import AdminLogout from "./AdminLogout";
 
 export default function AdminDashboard({ data }: { data: AdminData }) {
   return (
