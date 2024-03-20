489b8fd6693886b9e7dee769e71eae01931a60de
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-20 11:35:41 +0700

    Update Student.ts

--- src/hooks/Student.ts
+++ src/hooks/Student.ts
@@ -31,3 +31,36 @@ export function useStudentBookState() {
 
   return { state, dispatch };
 }
+
+export function useStudentCheckoutState() {
+  type State = Partial<{
+    searching: boolean;
+    searchTerm: string;
+    searchResult: typeof checkouts;
+  }>;
+
+  const { checkouts } = useContext(StudentDataContext);
+  const { state, dispatch } = useCustomReducer<State>({});
+  const searchResult = useMemo(() => {
+    const searchTerm = state.searchTerm;
+    if (searchTerm) {
+      return checkouts?.filter((checkout) =>
+        [
+          checkout.Copy.Book.title,
+          checkout.Copy.Book.author,
+          checkout.Copy.Book.isbn.toString(),
+          checkout.Copy.serial,
+        ]
+          .map((term) => term.toLowerCase())
+          .some((term) => term.includes(searchTerm.toLowerCase())),
+      );
+    }
+    return checkouts;
+  }, [checkouts, state.searchTerm]);
+
+  useEffect(() => {
+    dispatch({ type: "set", payload: { searchResult } });
+  }, [dispatch, searchResult]);
+
+  return { state, dispatch };
+}
