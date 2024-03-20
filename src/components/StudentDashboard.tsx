4507f8124e0103e621347369e7833bad862c0585
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-20 10:27:18 +0700

    Update StudentDashboard.tsx

--- src/components/StudentDashboard.tsx
+++ src/components/StudentDashboard.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { StudentData, StudentDataContext } from "@/context/Student";
 import { studentLogout } from "@/lib/action";
 import { Tab, TabList, TabPanels, Tabs } from "@chakra-ui/react";
 
@@ -7,19 +8,21 @@ import SharedLogout from "./SharedLogout";
 import StudentBook from "./StudentBook";
 import StudentCheckout from "./StudentCheckout";
 
-export default function StudentDashboard() {
+export default function StudentDashboard({ data }: { data: StudentData }) {
   return (
-    <Tabs isFitted className="!flex h-full flex-col">
-      <TabPanels className="flex-1">
-        <StudentBook />
-        <StudentCheckout />
-        <SharedLogout form={{ action: studentLogout }} />
-      </TabPanels>
-      <TabList className="flex-none">
-        <Tab>Book</Tab>
-        <Tab>Checkout</Tab>
-        <Tab>Logout</Tab>
-      </TabList>
-    </Tabs>
+    <StudentDataContext.Provider value={data}>
+      <Tabs isFitted className="!flex h-full flex-col">
+        <TabPanels className="flex-1">
+          <StudentBook />
+          <StudentCheckout />
+          <SharedLogout form={{ action: studentLogout }} />
+        </TabPanels>
+        <TabList className="flex-none">
+          <Tab>Book</Tab>
+          <Tab>Checkout</Tab>
+          <Tab>Logout</Tab>
+        </TabList>
+      </Tabs>
+    </StudentDataContext.Provider>
   );
 }
