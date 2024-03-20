2ed9792436f4703d1cf7b63c6ae3cc3203dd263f
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-20 10:03:54 +0700

    Create StudentDashboard.tsx

---
+++ src/components/StudentDashboard.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { studentLogout } from "@/lib/action";
+import { Tab, TabList, TabPanels, Tabs } from "@chakra-ui/react";
+
+import SharedLogout from "./SharedLogout";
+import StudentBook from "./StudentBook";
+import StudentCheckout from "./StudentCheckout";
+
+export default function StudentDashboard() {
+  return (
+    <Tabs isFitted className="!flex h-full flex-col">
+      <TabPanels className="flex-1">
+        <StudentBook />
+        <StudentCheckout />
+        <SharedLogout form={{ action: studentLogout }} />
+      </TabPanels>
+      <TabList className="flex-none">
+        <Tab>Book</Tab>
+        <Tab>Checkout</Tab>
+        <Tab>Logout</Tab>
+      </TabList>
+    </Tabs>
+  );
+}
