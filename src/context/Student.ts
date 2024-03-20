899164e4ac170b70f37e11989e396fab41a9360e
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-20 11:35:44 +0700

    Update Student.ts

--- src/context/Student.ts
+++ src/context/Student.ts
@@ -1,17 +1,23 @@
 import { createContext } from "react";
 
-import { useStudentBookState } from "@/hooks/Student";
+import { useStudentBookState, useStudentCheckoutState } from "@/hooks/Student";
 import { Prisma } from "@prisma/client";
 
 export type StudentData = Partial<{
   books: Prisma.BookGetPayload<{}>[];
-  checkouts: Prisma.CheckoutGetPayload<{}>[];
+  checkouts: Prisma.CheckoutGetPayload<{
+    include: { Copy: { include: { Book: true } } };
+  }>[];
 }>;
-
 export type StudentBookState =
   | ReturnType<typeof useStudentBookState>
+  | undefined;
+export type StudentCheckoutState =
+  | ReturnType<typeof useStudentCheckoutState>
   | undefined;
 
 export const StudentDataContext = createContext<StudentData>({});
 export const StudentBookStateContext =
   createContext<StudentBookState>(undefined);
+export const StudentCheckoutStateContext =
+  createContext<StudentCheckoutState>(undefined);
