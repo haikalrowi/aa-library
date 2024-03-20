6bd8e9c2c9df9a9a0e8091880b4727a2e3d469d8
Haikal Rowi <haikalbintang27@gmail.com>
2024-03-20 10:20:04 +0700

    Update page.tsx

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -1,17 +1,25 @@
 import SharedLogin from "@/components/SharedLogin";
 import StudentDashboard from "@/components/StudentDashboard";
 import { studentIsStudent, studentLogin } from "@/lib/action";
+import prisma from "@/lib/prisma";
 
 function Login() {
   return <SharedLogin form={{ action: studentLogin }} />;
 }
 
-function Dashboard() {
+function Dashboard(props: { studentId: string }) {
+  const books = prisma.book.findMany({
+    where: { Copy: { some: { available: true } } },
+  });
+  const checkouts = prisma.checkout.findMany({
+    where: { studentId: props.studentId },
+  });
+
   return <StudentDashboard />;
 }
 
 export default async function Student() {
-  const isStudent = await studentIsStudent();
+  const { isStudent, studentId } = await studentIsStudent();
 
-  return isStudent ? <Dashboard /> : <Login />;
+  return isStudent ? <Dashboard studentId={studentId} /> : <Login />;
 }
